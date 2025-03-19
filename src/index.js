0274493aaa44b32e041a683a13bf8728632c7b90
Ammu Lomy <ammulomy2017@gmail.com>
2025-03-19 10:49:08 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import InputText from './components/InputText';
 import InputNumber from './components/InputNumber';
 import InputCheckbox from './components/InputCheckbox';
+import './components/Style.css';
 
 
 export { InputText,InputNumber,InputCheckbox };
